17a68d24a0198a622b68c751ca9f0b3639273ae2
lananreen <lananreen@gmail.com>
2024-09-14 19:14:12 +0800

    Fixed the text in button

--- Interactivity_Tarlac.js
+++ Interactivity_Tarlac.js
@@ -3,7 +3,7 @@ const buttonContainer = document.getElementById('button-container');
 
 const button = document.createElement('button');
 
-button.textContent = 'Click Me!';
+button.textContent = 'Getting to know me!';
 
 button.addEventListener('click', function() {
     alert('A little information to get to know Lana!');
